74d58eecacb33b9c3c08a34c4404b55fda94ca6a
Mirko Matosin <llcoolmirko@hotmail.com>
2024-01-02 18:48:35 +0100

    reformat and adding new class object

--- masters/mastersclient.cpp
+++ masters/mastersclient.cpp
@@ -10,18 +10,30 @@
 // Visual studio needs extra define to use math constants
 #include <cmath>
 
+#include "iostream"
+
+
 using namespace KUKA::FRI;
+
 //******************************************************************************
 mastersclient::mastersclient(unsigned int jointMask, double freqHz,
                              double amplRad, double filterCoeff)
-    : _jointMask(jointMask), _freqHz(freqHz), _amplRad(amplRad),
-      _filterCoeff(filterCoeff), _offset(0.0), _phi(0.0), _stepWidth(0.0) {
-  printf("mastersclient initialized:\n"
-         "\tjoint mask: 0x%x\n"
-         "\tfrequency (Hz): %f\n"
-         "\tamplitude (rad): %f\n"
-         "\tfilterCoeff: %f\n",
-         jointMask, freqHz, amplRad, filterCoeff);
+        : _jointMask(jointMask), _freqHz(freqHz), _amplRad(amplRad),
+          _filterCoeff(filterCoeff), _offset(0.0), _phi(0.0), _stepWidth(0.0) {
+    printf("mastersclient initialized:\n"
+           "\tjoint mask: 0x%x\n"
+           "\tfrequency (Hz): %f\n"
+           "\tamplitude (rad): %f\n"
+           "\tfilterCoeff: %f\n",
+           jointMask, freqHz, amplRad, filterCoeff);
+    jointPos = new double[LBRState::NUMBER_OF_JOINTS];
+    for (int i = 0; i < LBRState::NUMBER_OF_JOINTS; i++) { jointPos[i] = 0.0; }
+    currentSampleTimeSec = 0;
+    currentSampleTimeNanoSec = 0;
+    prvSampleTimeSec = 0;
+    prvSampleTimeNanoSec = 0;
+    std::string xmlpath = "/home/mirko/OneDrive/codespace/fri_cmake/masters/exa_iiwa_dh_model.xml";
+    robotmdl = new robotModel(xmlpath);
 }
 
 //******************************************************************************
@@ -30,33 +42,33 @@ mastersclient::~mastersclient() {}
 //******************************************************************************
 void mastersclient::onStateChange(ESessionState oldState,
                                   ESessionState newState) {
-  LBRClient::onStateChange(oldState, newState);
-  // (re)initialize sine parameters when entering Monitoring
-  switch (newState) {
-  case IDLE: {
-    this->s_eSessionstate = "IDLE";
-    break;
-  }
-  case MONITORING_READY: {
-    _offset = 0.0;
-    _phi = 0.0;
-    _stepWidth = 2 * M_PI * _freqHz * robotState().getSampleTime();
-    this->printJointPos();
-    this->s_eSessionstate = "MONITORING_READY";
-    break;
-  }
-  case COMMANDING_WAIT: {
-    this->s_eSessionstate = "COMMANDING_WAIT";
-    break;
-  }
-  case COMMANDING_ACTIVE: {
-    this->s_eSessionstate = "COMMANDING_ACTIVE";
-    break;
-  }
-  default: {
-    break;
-  }
-  }
+    LBRClient::onStateChange(oldState, newState);
+    // (re)initialize sine parameters when entering Monitoring
+    switch (newState) {
+        case IDLE: {
+            this->s_eSessionstate = "IDLE";
+            break;
+        }
+        case MONITORING_READY: {
+            _offset = 0.0;
+            _phi = 0.0;
+            _stepWidth = 2 * M_PI * _freqHz * robotState().getSampleTime();
+            this->printJointPos();
+            this->s_eSessionstate = "MONITORING_READY";
+            break;
+        }
+        case COMMANDING_WAIT: {
+            this->s_eSessionstate = "COMMANDING_WAIT";
+            break;
+        }
+        case COMMANDING_ACTIVE: {
+            this->s_eSessionstate = "COMMANDING_ACTIVE";
+            break;
+        }
+        default: {
+            break;
+        }
+    }
 }
 void mastersclient::monitor() {
   // In waitForCommand(), the joint values have to be mirrored. Which is done,
