658c50f827f064395cfc5a6b82fced4072d3c703
Mirko Matošin <llcoolmirko@hotmail.com>
2024-01-25 21:18:13 +0000

    Ein paar Änderungen um mit VSCODE zu arbeiten

--- .docker/builddocker.sh
+++ .docker/builddocker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker buildx build -f="Dockerfile.BaseImage"  -t mmatosin/mybase ..
-docker buildx build -f="Dockerfile.rl" -t mmatosin/rl-app ..
-docker buildx build -f="Dockerfile.mastersApp" -t mmatosin/masters-app ..
+docker buildx build -f="Dockerfile.roblib" -t mmatosin/rl-app ..
+docker buildx build -f="Dockerfile.mastersAppRun" -t mmatosin/mastersapp-run ..
+docker buildx build -f="Dockerfile.mastersAppDev" -t mmatosin/mastersapp-dev ..
