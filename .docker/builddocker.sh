207eac5736b00d84e7f79f4b8296f1d4e943e120
Mirko <MightyMirko@users.noreply.github.com>
2024-11-15 08:55:43 +0100

    Update builddocker.sh

--- .docker/builddocker.sh
+++ .docker/builddocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker buildx build -f="Dockerfile.BaseImage"  -t mmatosin/mybase ..
-docker buildx build -f="Dockerfile.roblib" -t mmatosin/rl-app ..
-docker buildx build -f="Dockerfile.mastersAppRun" -t mmatosin/mastersapp-run ..
-docker buildx build -f="Dockerfile.mastersAppDev" -t mmatosin/mastersapp-dev ..
+docker buildx build -f="Dockerfile.BaseImage"  -t localhost/mybase ..
+docker buildx build -f="Dockerfile.roblib" -t localhost/rl-app ..
+docker buildx build -f="Dockerfile.mastersAppRun" -t localhost/mastersapp-run ..
+docker buildx build -f="Dockerfile.mastersAppDev" -t localhost/mastersapp-dev ..
